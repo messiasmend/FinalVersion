bdf6cd220adcbeb335515ceb87de09649e7bb34d
messiasmend <36993138+messiasmend@users.noreply.github.com>
2026-09-02 20:05:34 -0300

    Fix WebHider 4.7 metadata capture API

--- WebHider-4.7-Source/Sources/PHMetadataStore.h
+++ WebHider-4.7-Source/Sources/PHMetadataStore.h
@@ -6,6 +6,7 @@ NS_ASSUME_NONNULL_BEGIN
 @interface PHMetadataStore : NSObject
 
 + (void)captureSelectedElementFromWebView:(WKWebView *)webView;
++ (void)captureSelectedElementFromWebView:(WKWebView *)webView completion:(void (^)(BOOL captured))completion;
 + (void)synchronizeWithFilters;
 + (BOOL)hasPendingMetadata;
 + (nullable NSString *)nameForSelector:(NSString *)selector;
