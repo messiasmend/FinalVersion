70448c7956996c31282b4930f88fc3005d437dfe
messiasmend <36993138+messiasmend@users.noreply.github.com>
2026-09-02 18:52:51 -0300

    Harden WebHider 4.7 metadata synchronization

--- WebHider-4.7-Source/Sources/PHMetadataStore.h
+++ WebHider-4.7-Source/Sources/PHMetadataStore.h
@@ -7,6 +7,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 + (void)captureSelectedElementFromWebView:(WKWebView *)webView;
 + (void)synchronizeWithFilters;
++ (BOOL)hasPendingMetadata;
 + (nullable NSString *)nameForSelector:(NSString *)selector;
 
 @end
